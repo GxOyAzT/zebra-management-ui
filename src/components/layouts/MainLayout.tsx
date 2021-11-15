08b84f093669a0fdcd2c2276513c0e1c82c5fa8c
Jakub Koza <jakub.koza13@gmail.com>
2021-11-15 22:20:03 +0100

    Add timer for <Popup/>

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,9 +1,14 @@
+import { useContext } from 'react'
 import Popup from '../popup/Popup'
+import { popupContext } from '../../contexts/PopupContext'
 
 function MainLayout() {
+  const pop = useContext(popupContext)
   return (
     <div>
-      <Popup />
+      {
+        pop?.popup ? <Popup popupModel={pop.popup} closePopup={pop.setPopup}/> : <></>
+      }
     </div>
   )
 }
